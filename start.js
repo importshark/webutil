9b3ade286245f2417e86994e3a716e01c67f3bc8
importshark <87042591+importshark@users.noreply.github.com>
2022-06-19 13:37:58 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
-const name = process.cwd().split('/').pop();
-const data = require('../../servers.json')[name];
+const name = process.cwd().split('\\').pop();
+console.log(name);
+const data = require('../../servers.json').servers[name];
+console.log(data);
 const {JavaCaller} = require('java-caller');
 //Start the server
 const java = new JavaCaller({
     jar: 'server.jar',
+    minimumJavaVersion: data.javaversion,
+    maximumJavaVersion: data.javaversion,
 });
 async function run(){
 const { status, stdout, stderr } = await java.run(["-Xms" + data.minRAM, "-Xmx" + data.maxRAM]);
